fe7d03f456664db7be2230d581ad7ac964ecd91e
Victoire <vicmisanu63@gmail.com>
2025-06-26 11:13:21 +0100

    adding routing system

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,13 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const AuthController = () => import('../app/controllers/auth_controller.js')
+
 
 router.get('/', async () => {
   return {
-    hello: 'world',
+    message: "Welcome to Venu d'ailleurs API",
   }
 })
+router.post('/signin', [AuthController, 'authenticateUser'])
+router.post('/signup', [AuthController, 'registerAccountInfo'])
